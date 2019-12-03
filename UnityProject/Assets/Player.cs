7aa4068ac592d2e020d5df9c1457715d610b3d3f
jerry19950908 <57181340+jerry19950908@users.noreply.github.com>
2019-12-03 23:36:19 +0800

    作業(4)

---
+++ UnityProject/Assets/Player.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Header("血量")]
+    public float hp = 100;
+    [Header("攻擊")]
+    public float atk;
+    [Header("音源")]
+    public AudioSource aud ;
+    [Header("音效")]
+    public AudioClip soundatk;
+
+    [Header("殭屍")]
+    public Zombie zombie;
+
+    private void Attack()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            aud.PlayOneShot(soundatk, 2f);
+            atk = Random.Range(10f, 30f);
+            zombie.Hurt(atk); //殭屍.受到傷害 (玩家的atk)
+        }
+    }
+
+
+    private void Update()
+    {
+        Attack();
+    }
+
+
+    public void Hurt(float dam)  //這邊的dam等於僵屍的攻擊力
+    {
+        hp = hp - dam;
+        print("<color=blue>玩家受到傷害：" + dam + "</color>");
+        print("<color=blue>玩家剩下血量：" + hp + "</color>");
+
+        if (hp <= 0)
+        {
+            Dead();
+        }
+    }
+
+
+    private void Dead()
+    {
+        print("<color=blue>玩家死亡</color>");
+        
+
+    }
+
+
+}
---
+++ UnityProject/Assets/Zombie.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+public class Zombie: MonoBehaviour
+{
+    [Header("血量")]
+    public float hp = 50;
+    [Header("攻擊")]
+    public float atk;
+    [Header("音源")]
+    public AudioSource aud;
+    [Header("音效")]
+    public AudioClip soundatk;
+
+    [Header("玩家")]
+    public Player player;
+        
+
+    private void Attack()
+    {
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            aud.PlayOneShot(soundatk, 2f);
+            atk = Random.Range(20f, 60f);
+            player.Hurt(atk); //玩家.受到的傷害(殭屍的atk)
+        }
+    }
+
+
+
+    private void Update()
+    {
+        Attack();
+    }
+
+
+
+    public void Hurt(float dam) //這邊的dam 等於玩家的攻擊力
+    {
+        hp = hp - dam;
+        print("<color=red>殭屍受到傷害：" + dam + "</color>");
+        print("<color=red>殭屍受到傷害：" + hp + "</color>");
+
+        if (hp <= 0)
+        {
+            Dead();
+        }
+    }
+
+
+    private void Dead()
+    {
+        print("<color=red>殭屍死亡</color>");
+    }
+
+}
